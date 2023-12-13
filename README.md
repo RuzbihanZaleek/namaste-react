03b45c1db8167ec2f268e92e22f037a9f1f6081f
RuzbihanZaleek <ruzbihan.z@eyepax.com>
2023-12-13 22:17:18 +0530

    Episode 02

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React 🚀
+
+# Parcel
+    - Dev Build
+    - Local Server
+    - HMR - Hot Module Replacement - update the application code in real-time without requiring a full page reload
+    - File Watching Algorithms - written in C++
+    - Caching - Faster Builds
+    - Image Optimization
+    - Minification
+    - Bundling
+    - Compressing
+    - Consistent Hashing - distribute data across multiple nodes
+    - Code Splitting
+    - Differential Bundling - support older browsers
+    - Diagnostic
+    - Error Handling
+    - HTTPs
+    - Tree Shaking - remove unused code
+    - Different dev and production bundles
--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const reactHeading = React.createElement('h1', { id: 'heading' }, 'Hello World from React!');
 
 const reactRoot = ReactDOM.createRoot(document.getElementById('root2'));
@@ -19,7 +22,7 @@ reactRoot.render(reactHeading);
 
 const parent = React.createElement('div', { id: 'parent' }, [
   React.createElement('div', { id: 'child' }, [
-    React.createElement('h1', {}, 'I am an h1 Tag1'),
+    React.createElement('h1', {}, 'This is React 🚀'),
     React.createElement('h2', {}, 'I am an h2 Tag2'),
   ]),
   React.createElement('div', { id: 'child2' }, [
